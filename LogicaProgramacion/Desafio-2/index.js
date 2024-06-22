ea1f4e95996fe873aaca41d5edfef068fc4a8e88
Del Campo Fabian <fabydelcampo@hotmail.com>
2024-06-22 02:53:15 -0300

    Desafio 2 de logica completado

---
+++ LogicaProgramacion/Desafio-2/index.js
@@ -0,0 +1,40 @@
+//1- Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!". De lo contrario, muestra "¡Buena semana!".
+
+// let dia = prompt("Ingrese dia de la semana");
+
+// if(dia == "Sabado" || dia == "Domingo"){
+//     alert("¡Buen fin de semana!");
+// }else{
+//    alert("¡Buena semana!") ;
+// }
+
+//2- Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.
+
+// let numero = parseInt("Ingresar un numero");
+
+// if(numero>=0){
+//     alert("Positivo")
+// }else{
+//     alert("Negativo")
+// }
+
+//3- Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.".
+
+// let juego = parseInt(prompt("Ingresar puntuación"));
+
+// if(juego >=100){
+//     alert("¡Felicidades, has ganado!");
+// }else{
+//     alert("Intentalo nuevamente para ganar.");
+// }
+
+//4- Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
+
+// let saldo= 1000;
+// let mensaje= `saldo actual de su cuenta: ${saldo}`;
+
+//5- Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
+
+//let nombre= prompt("Ingrese su nombre");
+
+//alert(`Bienvenido... ${nombre}`);
